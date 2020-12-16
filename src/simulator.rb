2d5deb2d082d678c0846d947ceb1029f7665f791
Junichi Ito <jit@sonicgarden.jp>
2020-12-16 19:06:47 +0900

    Refactoring

--- src/simulator.rb
+++ src/simulator.rb
@@ -3,18 +3,15 @@ require 'native'
 
 class Simulator
   def simulate(total_count)
-    table = Hash.new {|h, k| h[k] = 0}
-    all_cards = (1..total_count).to_a
-    count = 0
+    table = Hash.new(0)
+    all_cards = [*1..total_count]
     results = []
     while table.keys.count < total_count
       this_time = all_cards.sample
       result = !table.key?(this_time)
       table[this_time] += 1
-      count += 1
-      results << {count: count, this_time: this_time, result: result, current_count: table.keys.count}
+      results << { count: results.size.succ, this_time: this_time, result: result, current_count: table.keys.count }
     end
-
     results
   end
 end
