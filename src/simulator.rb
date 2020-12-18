a90cbbefd9721777d7cd69402e9676ad016e6f5e
Junichi Ito <jit@sonicgarden.jp>
2020-12-18 19:31:12 +0900

    Use set instead of hash

--- src/simulator.rb
+++ src/simulator.rb
@@ -1,16 +1,17 @@
 require 'opal'
 require 'native'
+require 'set'
 
 class Simulator
   def self.simulate(number_of_types)
-    table = Hash.new(0)
+    table = Set[]
     all_cards = [*1..number_of_types]
     results = []
-    while table.keys.count < number_of_types
+    while table.size < number_of_types
       you_get = all_cards.sample
-      first_get = !table.key?(you_get)
-      table[you_get] += 1
-      results << { count_total: results.size.succ, you_get: you_get, first_get: first_get, count_distinct: table.keys.count }
+      first_get = !table.include?(you_get)
+      table << you_get
+      results << { count_total: results.size.succ, you_get: you_get, first_get: first_get, count_distinct: table.size }
     end
     results
   end
