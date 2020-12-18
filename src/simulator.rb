6b3d94e3ab333aae405778dfac15f67abfcbbc8c
Junichi Ito <jit@sonicgarden.jp>
2020-12-18 19:47:44 +0900

    Rename variables

--- src/simulator.rb
+++ src/simulator.rb
@@ -4,14 +4,14 @@ require 'set'
 
 class Simulator
   def self.simulate(number_of_types)
-    table = Set[]
-    all_cards = [*1..number_of_types]
+    set_of_cards = Set[]
+    all_types = [*1..number_of_types]
     results = []
-    while table.size < number_of_types
-      you_get = all_cards.sample
-      first_get = !table.include?(you_get)
-      table << you_get
-      results << { count_total: results.size.succ, you_get: you_get, first_get: first_get, count_distinct: table.size }
+    while set_of_cards.size < number_of_types
+      you_get = all_types.sample
+      first_get = !set_of_cards.include?(you_get)
+      set_of_cards << you_get
+      results << { count_total: results.size.succ, you_get: you_get, first_get: first_get, count_distinct: set_of_cards.size }
     end
     results
   end
